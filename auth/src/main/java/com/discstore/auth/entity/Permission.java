196c665ae37036da2101448dfbb961ad2803a7c9
Lindemberg Holanda <lindemberg.holanda@gmail.com>
2021-06-09 22:09:28 -0300

    config authentication and gateway zuul

---
+++ auth/src/main/java/com/discstore/auth/entity/Permission.java
@@ -0,0 +1,45 @@
+package com.discstore.auth.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.springframework.security.core.GrantedAuthority;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Table(name = "permission")
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+public class Permission implements GrantedAuthority, Serializable {
+	
+	private static final long serialVersionUID = -228850481997644287L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(name = "description", nullable = false)
+	private String description;
+
+	@Override
+	public String getAuthority() {
+		return description;
+	}
+
+}
--- gateway/src/main/java/com/discstore/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/discstore/gateway/GatewayApplication.java
@@ -1,15 +1,17 @@
-package com.discstore.payment;
+package com.discstore.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class PaymentApplication {
+@EnableZuulProxy
+public class GatewayApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PaymentApplication.class, args);
+		SpringApplication.run(GatewayApplication.class, args);
 	}
 
 }
